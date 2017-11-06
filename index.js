e9704d094e9ab4338a36ca22b62b2adcb2c608a7
Soha Rahman <studentinsha12@gmail.com>
2017-11-06 08:56:18 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,5 +16,13 @@ function nowServing(katzDeliLine) {
 function currentLine(katzDeliLine) {
   if(katzDeliLine.length===0)
     return "The line is currently empty."
-  
+  else {
+    var s=; 
+    var x='';
+    for(var i=0;i<katzDeliLine.length;i++) {
+      x=x+`${i}.${katzDeliLine[i]}${i===katzDeliLine.length-1?'':','}`;
+    }
+    return "The line is currently: "+x;
+    
+  }
 }
