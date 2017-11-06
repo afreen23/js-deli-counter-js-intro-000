aab05ce5bc5e074848a71797b98757507b7cec96
Soha Rahman <studentinsha12@gmail.com>
2017-11-06 08:44:41 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ function nowServing(katzDeliLine) {
   if(katzDeliLine.length===0)
    return "There is nobody waiting to be served!";
   else {
-    console.log(`Currently serving ${katzDeliLine[0]}.`);
-    katzDeliLine.shift();
+        var person=katzDeliLine.shift();
+    return `Currently serving ${person}.`;
   }
 }
